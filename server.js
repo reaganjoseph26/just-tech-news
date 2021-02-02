041b4b78eef5a47df8b61122b18f9a6c2ddf1c89
Racer <reaganjoseph26@yahoo.com>
2021-02-02 11:54:51 -0500

    files updated

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//take all of the contents of a folder and serve them as static assets.
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on routes
 app.use(routes);
